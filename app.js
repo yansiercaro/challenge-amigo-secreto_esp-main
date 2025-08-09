6311afffdffe824f557644ac75c0a112240f220a
yansiercaro <joseyansiercarovarona@gmail.com>
2025-08-09 15:03:40 -0400

    Crea array para almacenar

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-123
-
+//Crea un array para almacenar los nombres.
+let amigosSorteo = [];
